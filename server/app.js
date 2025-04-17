47aea6657e231d95faf34625db9690a57a98c44d
unknown <telysemiller3@gmail.com>
2025-04-16 21:03:04 -0500

    updated

---
+++ server/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4040;
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+
+const cors = (request, response, next) => {
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  response.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+
+app.get("/status", (request, response) => {
+  response.json({ message: "Service healthy" });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
