c9654e82d41f23ba81ea9654edb690a13e6fcbf8
unknown <telysemiller3@gmail.com>
2025-04-28 12:05:58 -0500

    week 12 updates

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-  <footer>&copf; 2025 <a href="https://Elevate.com/">Elevate</a></footer>
+  <div class="footer">
+    <footer>&copf; 2025 <a href="https://Elevate.com/">Elevate</a></footer>
+  </div>
 `;
